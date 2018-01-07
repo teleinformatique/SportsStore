562e837fa9c3627b444e1fe83d9aac565f902c37
Khadim Sarr <khadimsarr@outlook.com>
2018-01-07 18:52:43 +0000

    creating the database class

---
+++ SportStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SportStore.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
